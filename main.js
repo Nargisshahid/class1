6d6ce0f2fac2b0dc801041cd43a8f034eec8df60
Nargisshahid <165659739+Nargisshahid@users.noreply.github.com>
2024-04-01 15:49:58 +0500

    these are my first two files

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+console.log("hello shahid");
+console.log("my name is nargis");
+console.log("very good job");
+console.log("hello baby how are you");
+console.log("hello rida how are you");
